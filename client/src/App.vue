dedfda005065aed3bf984b5668360de485771421
ashwin97 <ashwix1997@gmail.com>
2020-09-22 17:40:12 -0700

    Add client folder for frontend

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <TreeViewer />
+  </div>
+</template>
+
+<script>
+import TreeViewer from './components/TreeViewer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TreeViewer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
