01c1942b4993b719ac727ac76d81d2f7e9c21f85
ashwin97 <ashwix1997@gmail.com>
2020-09-22 23:18:34 -0700

    config newpage

--- client/src/views/trees/NewPage.vue
+++ client/src/views/trees/NewPage.vue
@@ -1,32 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>new page</h1>
+  <div class="">
+    <h2>Add Tree</h2>
+    <div class=row>
+        <div class="col">
+            <input v-model="genus" placeholder="genus">
+        </div>
+    </div>
+    <div class=row>
+        <div class="col">
+            <input v-model="image" placeholder="image url">
+        </div>
+    </div>
+    <div class=row>
+        <div class="col">
+            <input v-model="weight" placeholder="weight(kg)">
+        </div>
+    </div>
+    <div class=row>
+        <div class="col">
+            <input v-model="height" placeholder="height(m)">
+        </div>
+    </div>
+    <div class=row>
+        <div class="col">
+            <input v-model="longitude" placeholder="longitude">
+        </div>
+    </div>
+    <div class=row>
+        <div class="col">
+            <input v-model="latitude" placeholder="latitude">
+        </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'new-page',
-  props: {
-    msg: String
+  data(){
+      return{
+          genus: '',
+          image: '',
+          weight: 0,
+          height: 0,
+          longitude:'',
+          latitude:''
+      }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+h2 {
+  margin-bottom:20px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.col{
+    margin-bottom:10px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
