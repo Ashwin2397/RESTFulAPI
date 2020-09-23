2b7582714bb02fa66a2f704b68e8a0fa167497f3
ashwin97 <ashwix1997@gmail.com>
2020-09-22 18:53:16 -0700

    Server runs

---
+++ server/app.js
@@ -0,0 +1,34 @@
+//Dependencies
+require("dotenv").config();
+var mongoose=require("mongoose"),
+    express = require("express"),
+    User    =require("./models/user.js"),
+    Tree    =require("./models/tree.js"),
+    cors    =require("cors"),
+    app     =express();
+
+//Routes
+var treeRoutes  =require("./routes/trees.js"),
+    indexRoutes =require("./routes/index.js");
+
+// Connect to mongodb server
+var url="mongodb://127.0.0.1:27017/trees";
+mongoose.connect(url,{useNewUrlParser: true});
+mongoose.set('useUnifiedTopology', true);
+
+//Check if connection to db is successfull
+var db = mongoose.connection
+db.once('open', _ => {
+  console.log('Database connected:', url)
+})
+db.on('error', err => {
+  console.error('connection error:', err)
+})
+
+//Connect app to routes
+app.use("/",indexRoutes);
+
+
+app.listen(3000,function(){
+    console.log("Server has started");
+});
