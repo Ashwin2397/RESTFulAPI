3c39ea692bc8b098d49d89befb5a39bbacaf2710
ashwin97 <ashwix1997@gmail.com>
2020-09-22 18:53:42 -0700

    Add index routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 var express =require("express"),
-    router  =express.router(),
+    router  =express.Router(),
     User    =require("../models/user.js");
     
+//Landing Page
 router.get("/",function(req,res){
     res.send("Hello World!")
 });
 
+//No User routes
 module.exports =router;
