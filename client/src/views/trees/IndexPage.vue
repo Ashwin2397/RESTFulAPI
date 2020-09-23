54321aad38514c1d86e77374e1475eb3fbc24dfb
ashwin97 <ashwix1997@gmail.com>
2020-09-22 23:18:01 -0700

    config indexpage

--- client/src/views/trees/IndexPage.vue
+++ client/src/views/trees/IndexPage.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="hello">
-    <h1>index page</h1>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Trees</h1>
+        <p class="lead">A brief lists of all trees in Victoria</p>
+      </div>
+    </div>
+    <a id="create" class="btn btn-success" href="./new">Create new tree</a>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          1 of 2
+        </div>
+        <div class="col">
+          2 of 2
+        </div>
+        <div class="col">
+          3 of 3
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -26,7 +46,19 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-a {
-  color: #42b983;
+.container{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+}
+a{
+  text-align: right;
+}
+
+.col{
+  margin-top: 20px;
+  background:pink;
 }
 </style>
